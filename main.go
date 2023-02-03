a52c46f6fd4c4e670b24ef45839aa249701bc97f
Henrique Migotto <hmigotto@gmail.com>
2023-02-03 11:53:03 -0300

    Pacote models adicionado

--- main.go
+++ main.go
@@ -4,14 +4,7 @@ import (
 	"net/http"
 	"text/template"
 
-	"context"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-
-	"github.com/Hmigotto/goprofessor/db"
-	"go.mongodb.org/mongo-driver/bson"
+	"github.com/Hmigotto/goprofessor/models"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -19,65 +12,14 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func main() {
 
 	http.HandleFunc("/", index)
-	http.HandleFunc("/create", create)
+	http.HandleFunc("/create", models.Create)
 	http.ListenAndServe(":8000", nil)
 
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 
-	produtos := display()
+	produtos := models.Display()
 
 	temp.ExecuteTemplate(w, "Index", produtos)
-}
-
-type Produto struct {
-	Nome       string
-	Descricao  string
-	Preco      string
-	Quantidade string
-}
-
-func create(w http.ResponseWriter, r *http.Request) { //Esta linha define a função create com dois argumentos w e r do tipo http.ResponseWriter e *http.Request respectivamente. Esses dois tipos são usados para gerenciar a resposta da requisição HTTP.
-
-	// Read the body of the POST request
-	body, err := ioutil.ReadAll(r.Body)
-
-	// Unmarshal the body into a Produto struct
-
-	var produto Produto
-	err = json.Unmarshal(body, &produto)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create a person document
-	_, err = db.ConectaComBancoDeDadosColectionPeople().InsertOne(context.TODO(), produto) //Esta linha insere o documento person na coleção people. O valor de retorno é armazenado na variável err.
-
-	fmt.Println("Inserted person:", produto) //Esta linha imprime na resposta HTTP a mensagem "Inserted person" e os valores do documento person inserido.
-
-}
-
-func display() []Produto {
-	produtos := []Produto{}
-
-	// Get all people from the database
-	cursor, err := db.ConectaComBancoDeDadosColectionPeople().Find(context.TODO(), bson.D{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	for cursor.Next(context.TODO()) {
-		var produto Produto
-		err := cursor.Decode(&produto)
-		if err != nil {
-			log.Fatal(err)
-		}
-		produtos = append(produtos, produto)
-	}
-	if err := cursor.Err(); err != nil {
-		log.Fatal(err)
-	}
-	cursor.Close(context.TODO())
-
-	return produtos
 }
---
+++ models/produto.go
@@ -0,0 +1,65 @@
+package models
+
+import (
+	"net/http"
+
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/Hmigotto/goprofessor/db"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      string
+	Quantidade string
+}
+
+func Create(w http.ResponseWriter, r *http.Request) { //Esta linha define a função create com dois argumentos w e r do tipo http.ResponseWriter e *http.Request respectivamente. Esses dois tipos são usados para gerenciar a resposta da requisição HTTP.
+
+	// Read the body of the POST request
+	body, err := ioutil.ReadAll(r.Body)
+
+	// Unmarshal the body into a Produto struct
+
+	var produto Produto
+	err = json.Unmarshal(body, &produto)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a person document
+	_, err = db.ConectaComBancoDeDadosColectionPeople().InsertOne(context.TODO(), produto) //Esta linha insere o documento person na coleção people. O valor de retorno é armazenado na variável err.
+
+	fmt.Println("Inserted person:", produto) //Esta linha imprime na resposta HTTP a mensagem "Inserted person" e os valores do documento person inserido.
+
+}
+
+func Display() []Produto {
+	produtos := []Produto{}
+
+	// Get all people from the database
+	cursor, err := db.ConectaComBancoDeDadosColectionPeople().Find(context.TODO(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for cursor.Next(context.TODO()) {
+		var produto Produto
+		err := cursor.Decode(&produto)
+		if err != nil {
+			log.Fatal(err)
+		}
+		produtos = append(produtos, produto)
+	}
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+	cursor.Close(context.TODO())
+
+	return produtos
+}
